52dab7e6454e0a6ac98f99b15b653547bfd591ff
FitForAKing47 <natethetate90@gmail.com>
2014-01-10 13:37:08 -0700

    project
    we created it

---
+++ src/array/controller/ArrayController.java
@@ -0,0 +1,61 @@
+package array.controller;
+
+import array.model.Cookie;
+
+public class ArrayController
+{
+	private int [][] randomNumbers;
+	private Cookie [][] cookieTypes;
+	private String [][] cookieNames;
+	
+	
+	public ArrayController()
+	{
+		randomNumbers = new int [5][3];
+		cookieTypes = new Cookie [6][4];
+		cookieNames = new String [4][4];
+	}
+
+	
+	public int[][] getRandomNumbers()
+	{
+		return randomNumbers;
+	}
+
+
+	public Cookie[][] getCookieTypes()
+	{
+		return cookieTypes;
+	}
+
+
+	public String[][] getCookieNames()
+	{
+		return cookieNames;
+	}
+
+
+	public void setRandomNumbers(int[][] randomNumbers)
+	{
+		this.randomNumbers = randomNumbers;
+	}
+
+
+	public void setCookieTypes(Cookie[][] baconTypes)
+	{
+		this.cookieTypes = cookieTypes;
+	}
+
+
+	public void setCookieNames(String[][] cookieNames)
+	{
+		this.cookieNames = cookieNames;
+	}
+
+
+	public void start()
+	{
+		
+	}
+	
+}
---
+++ src/array/controller/ArrayRunner.java
@@ -0,0 +1,16 @@
+package array.controller;
+
+
+public class ArrayRunner
+{
+
+
+	public static void main(String[] args)
+	{
+		ArrayController myController = new ArrayController();
+		myController.start();
+
+	}
+
+}
+
